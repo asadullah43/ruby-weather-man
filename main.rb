dcb7fa9f2d09bc2bd8896cef670e9effbf7822d6
asadullah <asad.rehman@devsinc.com>
2022-03-29 23:51:07 +0500

    convert main file to class

--- main.rb
+++ main.rb
@@ -3,44 +3,53 @@
 require './weather_man_q2'
 require './weather_man_q1'
 require './weather_man_q3'
+# Main class
+class MainClass
+  def showing_result(choice, year_number, month, city)
+    case choice
+    when '-e'
+      obj_task_one = TaskOne.new(year_number, city)
+      obj_task_one.printing_data
+    when '-a'
+      obj_task_two = TaskTwo.new(year_number, month, city)
+      obj_task_two.printing_data
+    when '-c'
+      obj_task_three = TaskThree.new(year_number, month, city)
+      obj_task_three.barchart_temp('three')
+    when '-d'
+      obj_task_three = TaskThree.new(year_number, month, city)
+      obj_task_three.barchart_temp('four')
+    else
+      puts 'Wrong Input'
+    end
+  end
 
-def month_hash(year_number)
-  months = {
-    '1' => 'Jan',
-    '2' => 'Feb',
-    '3' => 'Mar',
-    '4' => 'Apr',
-    '5' => 'May',
-    '6' => 'Jun',
-    '7' => 'Jul',
-    '8' => 'Aug',
-    '9' => 'Sep',
-    '10' => 'Oct',
-    '11' => 'Nov',
-    '12' => 'Dec'
-  }
-  months[year_number]
-end
-input = ARGV
-choice = input[0]
-city = input[2]
-year = input[1].split('/')
-year_number = year[0]
-month = month_hash(year[1])
-case choice
-when '-e'
-  obj_task_one = TaskOne.new(year_number, city)
-  obj_task_one.printing_data
-when '-a'
-  obj_task_two = TaskTwo.new(year_number, month, city)
-  obj_task_two.printing_data
+  def month_hash(year_number)
+    months = {
+      '1' => 'Jan',
+      '2' => 'Feb',
+      '3' => 'Mar',
+      '4' => 'Apr',
+      '5' => 'May',
+      '6' => 'Jun',
+      '7' => 'Jul',
+      '8' => 'Aug',
+      '9' => 'Sep',
+      '10' => 'Oct',
+      '11' => 'Nov',
+      '12' => 'Dec'
+    }
+    months[year_number]
+  end
 
-when '-c'
-  obj_task_three = TaskThree.new(year_number, month, city)
-  obj_task_three.barchart_temp('three')
-when '-d'
-  obj_task_three = TaskThree.new(year_number, month, city)
-  obj_task_three.barchart_temp('four')
-else
-  puts 'Wrong Input'
+  def input
+    input = ARGV
+    choice = input[0]
+    city = input[2]
+    year = input[1].split('/')
+    year_number = year[0]
+    month = month_hash(year[1])
+    showing_result(choice, year_number, month, city)
+  end
 end
+MainClass.new.input
