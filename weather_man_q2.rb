d530845f7bccb98a967d1ff7b840e580dbebe227
asadullah <asad.rehman@devsinc.com>
2022-03-29 18:53:30 +0500

    remove spaces

--- weather_man_q2.rb
+++ weather_man_q2.rb
@@ -5,16 +5,16 @@ require 'csv'
 class TaskTwo
   def initialize(year, month, city)
     @contents = CSV.parse(File.read("./#{city}/#{city}_#{year}_#{month}.txt"), headers: true, converters: :numeric)
+  end
 
-    def calculate_avg(value)
-      length = @contents.by_col[value].length
-      (@contents.by_col[value].compact.inject(0) { |sum, number| sum + number }) / length
-    end
+  def calculate_avg(value)
+    length = @contents.by_col[value].length
+    (@contents.by_col[value].compact.inject(0) { |sum, number| sum + number }) / length
+  end
 
-    def printing_data
-      puts "Highest Average:#{calculate_avg(1)}C"
-      puts "Lowest Average:#{calculate_avg(3)}C"
-      puts "Average Humadity:#{calculate_avg(7)}%"
-    end
+  def printing_data
+    puts "Highest Average:#{calculate_avg(1)}C"
+    puts "Lowest Average:#{calculate_avg(3)}C"
+    puts "Average Humadity:#{calculate_avg(7)}%"
   end
 end
